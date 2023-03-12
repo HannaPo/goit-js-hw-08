fe144fdd2550a4d24cda232fb9937e47a5984bd2
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-03-12 11:26:45 +0200

    Task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+function getCurrentTime (e) {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
 
